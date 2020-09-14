252cb9dcf364be303a1e1ad65174246b37bc4d15
thiago <thiago@adven.tech>
2020-09-14 12:41:57 -0300

    Inicialização

---
+++ PaymentContext.Domain/Entities/Student.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Student
+    {
+        private IList<Subscription> _subscription;
+        public Student (string firstname, string lastname, string document, string email)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Document = document;
+            Email = email;
+            _subscription = new List<Subscription>();
+        }
+
+        public string FirstName { get; private set; }   
+        public string LastName { get; private set; }   
+        public string Document { get; private set; }   
+        public string Email { get; private set; }   
+        public string Address { get; private set; }
+        public IReadOnlyCollection<Subscription> Subscriptions { get{ return _subscription.ToArray(); } }
+
+        public void AddSubscription(Subscription subscription)
+        {
+            foreach (var sub in Subscriptions)
+                sub.Active = false;
+
+            _subscription.Add(subscription);
+        }
+    }
+    
+}
